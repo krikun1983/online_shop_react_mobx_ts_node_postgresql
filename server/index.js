0d87ad25773e38ade81d0ea1c659e9da4faadfe9
Stanislav <akhmadeevst@gmail.com>
2022-03-22 19:20:32 +0400

    feat: add models db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 import 'dotenv/config';
 import express from "express";
 import sequelize from './db.js';
+import models from './models/models.js';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.status(200).json({ message: 'Я работаю!!!' });
+});
 
 const start = async () => {
   try {
