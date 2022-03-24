665451b0a891a132cda61133cabb5a050b0efaff
Stanislav <akhmadeevst@gmail.com>
2022-03-24 12:12:26 +0400

    feat: add device

--- server/routes/controllers/deviceController.js
+++ server/routes/controllers/deviceController.js
@@ -17,16 +17,16 @@ class DeviceController {
 
       const device = await models.Device.create({ name, price, brandId, typeId, img: fileName });
 
-      // if (info) {
-      //   info = JSON.parse(info)
-      //   info.forEach(i =>
-      //     models.DeviceInfo.create({
-      //       title: i.title,
-      //       description: i.description,
-      //       deviceId: device.id
-      //     })
-      //   )
-      // }
+      if (info) {
+        info = JSON.parse(info)
+        info.forEach(i =>
+          models.DeviceInfo.create({
+            title: i.title,
+            description: i.description,
+            deviceId: device.id
+          })
+        )
+      }
 
       return response.json(device)
     } catch (e) {
@@ -35,10 +35,36 @@ class DeviceController {
 
   }
   async getAll(request, response) {
+    let { brandId, typeId, limit, page } = request.query;
+    limit = limit || 10;
+    page = page || 1;
+    let offset = page * limit - limit;
+    let devices;
+
+    if (!brandId && !typeId) {
+      devices = await models.Device.findAndCountAll({ limit, offset });
+    }
+    if (brandId && !typeId) {
+      devices = await models.Device.findAndCountAll({ where: { brandId }, limit, offset });
+    }
+    if (!brandId && typeId) {
+      devices = await models.Device.findAndCountAll({ where: { typeId }, limit, offset });
+    }
+    if (brandId && typeId) {
+      devices = await models.Device.findAndCountAll({ where: { brandId, typeId, limit, offset } });
+    }
+    return response.json(devices);
 
   }
   async getOneById(request, response) {
-
+    const { id } = request.params
+    const device = await models.Device.findOne(
+      {
+        where: { id },
+        include: [{ model: models.DeviceInfo, as: 'info' }]
+      },
+    )
+    return response.json(device);
   }
 }
 
